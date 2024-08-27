dc684c03fbcfe03e628fe0d2d10e2b4e13c7f6df
Telmo Fernandes <telmorf@live.com>
2024-08-27 21:04:34 +0100

    1st. Github Pages(correct-link-name)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/P015-MoviesLibraryTMDM-ReactAPI.git",
+  base: "/P015-MoviesLibraryTMDB-ReactAPI.git",
 })

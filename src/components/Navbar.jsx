71e283fffa26488aa7fa230b4337aeabcf1c9cd4
Telmo Fernandes <telmorf@live.com>
2024-08-28 14:45:46 +0100

    search Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import './Navbar.css'
 
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { BiAlarm, BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
 
 
 const Navbar = () => {
+  const [search, setSearch] = useState('')
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //console.log(search)
+    if (!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  }
+
   return (
 
     <nav id='navbar'>
@@ -17,8 +30,8 @@ const Navbar = () => {
         <Link to='/about'><BiAlarm />About</Link>
       </h2>
 
-      <form>
-        <input type="text" placeholder='Procurar um filme' />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Procurar um filme' onChange={(e) => setSearch(e.target.value)} value={search} />
         <button type='submit'>
           <BiSearchAlt2 />
         </button>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import MovieCard from '../components/MovieCard'
+import './MoviesGrid.css'
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
+
 
 const Search = () => {
+  const [searchParms] = useSearchParams()
+
+  const [movies, setMovies] = useState([])
+  const query = searchParms.get('q')
+  //console.log(query)
+  //console.log([searchParms])
+
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    //console.log(data.results)
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+    //console.log(topRatedUrl);
+    getSearchedMovies(searchWithQueryURL);
+  }, [query]);
+
   return (
-    <div>
-      Search
+
+    <div className="container">
+      <h2 className="title">Resultados para:
+        <span className='query-text'> {query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies <= 0 && <p>Carregando...</p>}
+        {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   )
 }
